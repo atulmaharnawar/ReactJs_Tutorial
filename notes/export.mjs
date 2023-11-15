3ff96745933aae3246a8c59c4f5efc1a71f2ef96
atulmaharnawar <amaharnavar21@gmail.com>
2023-11-15 08:18:10 +0530

    Saving ReactJsTut

---
+++ notes/export.mjs
@@ -0,0 +1,9 @@
+const a = "Harry";
+const b = "Rohan";
+const c = "Aakash";
+const d = "Divya";
+
+export default c; //default export
+export { a }; //named export
+export { b };  //named export
+export { d };  //named export
---
+++ notes/export2.mjs
@@ -0,0 +1,8 @@
+import ui, { a, b, d } from './export.mjs';
+
+console.log(ui); //default
+
+//named
+console.log(a);
+console.log(b);
+console.log(d);
